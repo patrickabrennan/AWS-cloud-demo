433945adc3c5c437f536b114260b2e6f0b1fbd30
Patrick Brennan <patrickabrennan@gmail.com>
2023-10-22 19:46:35 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -31,8 +31,8 @@ resource "aws_security_group" "sg" {
   }
 
   egress {
-    from_port        = 22, 80, 443
-    to_port          = 22, 80, 443
+    from_port        = 0
+    to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
