4d4ddcd70a5676e0293fa5a535b5c72708f2f453
Patrick Brennan <patrickabrennan@gmail.com>
2023-10-22 19:31:01 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -5,13 +5,14 @@ resource "aws_security_group" "sg" {
 
   ingress {
     description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
+    from_port        = 22, 80, 443
+    to_port          = 22, 80, 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
+/*
   ingress {
     description      = "HTTP from VPC"
     from_port        = 80
@@ -29,10 +30,10 @@ resource "aws_security_group" "sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
+*/
   egress {
-    from_port        = 0
-    to_port          = 0
+    from_port        = 22, 80, 443
+    to_port          = 22, 80, 443
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
