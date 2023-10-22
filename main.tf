bc01eeea1a664fce1de913fed743de83a7435d9b
Patrick Brennan <patrickabrennan@gmail.com>
2023-10-22 15:39:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,6 +113,12 @@ resource "aws_instance" "web" {
   tags = {
     Name = "web_instance"
     Environment = "prod"
+    Name = "Patrick's Maps Demo" 
+    ttl = "7 Days"
+    owner = "Pat Brennan"
+    se-region = East-R2 
+    purpose = "Demo including Google Maps Application with location capability"
+   terraform = "Used to deploy"
   }
 
   volume_tags = {
