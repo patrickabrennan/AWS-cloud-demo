49523738bcbdabdc02468e6887865cc2e3e682a1
Patrick Brennan <patrickabrennan@gmail.com>
2023-10-23 14:01:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ provider "google" {
 
 # Create a VPC
 resource "aws_vpc" "app_vpc" {
-  cidr_block = var.aws_vpc_cidr
+  cidr_block = "${var.aws_vpc_cidr}"
   enable_dns_hostnames = var.enable_dns_hostnames
 
   tags = {
