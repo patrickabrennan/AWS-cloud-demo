fef3e648ba4177c521c6eac89c126d3dbe7d64b9
Patrick Brennan <patrickabrennan@gmail.com>
2023-10-22 22:20:09 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -2,16 +2,41 @@ resource "aws_security_group" "sg" {
   name        = "allow_ssh_http"
   description = "Allow ssh http https inbound traffic"
   vpc_id      = aws_vpc.app_vpc.id
+}
 
-  ingress {
+resource "aws_security_group_rule allow_ssh {
+    type            = ingress 
     description      = "SSH from VPC"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
+    security_group_id = "aws_security_group.sg.id"
   }
 
+resource "aws_security_group_rule allow_http {
+    type            = ingress 
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    security_group_id = "aws_security_group.sg.id"
+  }
+
+resource "aws_security_group_rule allow_https {
+    type            = ingress 
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    security_group_id = "aws_security_group.sg.id"
+  }
+/*
   ingress {
     description      = "HTTP from VPC"
     from_port        = 80
@@ -19,6 +44,7 @@ resource "aws_security_group" "sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
+    security_group_id = "aws_security_group.sg.id"
   }
 
   ingress {
@@ -28,8 +54,9 @@ resource "aws_security_group" "sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-  }
-
+    security_group_id = "aws_security_group.sg.id"  
+}
+*/
   egress {
     from_port        = 0
     to_port          = 0
