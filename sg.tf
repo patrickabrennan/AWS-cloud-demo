8645a4e11727a60e9e7310b89ffe90b9681df1b8
Patrick Brennan <patrickabrennan@gmail.com>
2023-10-22 23:11:47 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -2,52 +2,16 @@ resource "aws_security_group" "sg" {
   name        = "allow_ssh_http"
   description = "Allow ssh http https inbound traffic"
   vpc_id      = aws_vpc.app_vpc.id
-}
 
-resource "aws_security_group_rule" "allow_ssh" {
-    type            = "ingress" 
+  ingress {
     description      = "SSH from VPC"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-    security_group_id = "aws_security_group.sg.id"
   }
 
-resource "aws_security_group_rule" "allow_http" {
-    type            = "ingress"
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-    security_group_id = "aws_security_group.sg.id"
-  }
-
-resource "aws_security_group_rule" "allow_https" {
-    type            = "ingress" 
-    description      = "HTTPS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-    security_group_id = "aws_security_group.sg.id"
-  }
-
-resource "aws_security_group_rule" "egress-all" {
-    type            = "egress" 
-    description      = "Allow all traffic out from VPC"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-    security_group_id = "aws_security_group.sg.id"
-}
-/*
   ingress {
     description      = "HTTP from VPC"
     from_port        = 80
@@ -55,7 +19,6 @@ resource "aws_security_group_rule" "egress-all" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-    security_group_id = "aws_security_group.sg.id"
   }
 
   ingress {
@@ -65,8 +28,7 @@ resource "aws_security_group_rule" "egress-all" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
-    security_group_id = "aws_security_group.sg.id"  
-}
+  }
 
   egress {
     from_port        = 0
@@ -79,6 +41,4 @@ resource "aws_security_group_rule" "egress-all" {
   tags = {
     Name = "allow_ssh_http"
   }
-*/
-  
-
+}
