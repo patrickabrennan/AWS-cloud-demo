e11981841914822129e380fe1aec524c56cab387
Patrick Brennan <patrickabrennan@gmail.com>
2023-10-23 20:57:43 -0400

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -8,8 +8,8 @@ resource "aws_security_group" "sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [var.aws_vpc_cidr]
-    #cidr_blocks      = ["0.0.0.0/0"]
+    #cidr_blocks      = [var.aws_vpc_cidr]
+    cidr_blocks      = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -18,8 +18,8 @@ resource "aws_security_group" "sg" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [var.aws_vpc_cidr]
-    #cidr_blocks      = ["0.0.0.0/0"]
+    #cidr_blocks      = [var.aws_vpc_cidr]
+    cidr_blocks      = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -28,8 +28,8 @@ resource "aws_security_group" "sg" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [var.aws_vpc_cidr]
-    #cidr_blocks      = ["0.0.0.0/0"]
+    #cidr_blocks      = [var.aws_vpc_cidr]
+    cidr_blocks      = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -37,8 +37,8 @@ resource "aws_security_group" "sg" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = [var.aws_vpc_cidr]
-    #cidr_blocks      = ["0.0.0.0/0"]
+    #cidr_blocks      = [var.aws_vpc_cidr]
+    cidr_blocks      = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = ["::/0"]
   }
 
